7321c5aaa346068c05b90467dc386e5e4ba4ab8c
Sarpkaya Ture <sarpkayature@gmail.com>
2022-03-29 00:08:43 +0300

    Add Flatlist

--- src/components/ResultListItem/ResultListItem.js
+++ src/components/ResultListItem/ResultListItem.js
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList, Image } from "react-native";
 import Container from "../Container/Container";
 import { styles } from "./ResultListItemStyles";
 import BusinessService from "../../api/business.service";
 
 const ResultListItem = ({ route }) => {
   const { id } = route.params;
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState([]);
 
   const getDetail = async () => {
     const response = await BusinessService.get(id);
     setResults(response);
   };
+  console.log(results);
 
   useEffect(() => {
     getDetail();
@@ -20,9 +21,14 @@ const ResultListItem = ({ route }) => {
   return (
     <Container>
       <Text>{results.name}</Text>
-      <Text>{results.location.address1}</Text>
-      <Text>{results.location.state}</Text>
-      <Text>{results.location.city}</Text>
+      <Text>{results.phone}</Text>
+      <FlatList
+        keyExtractor={(result) => result.photos}
+        data={results.photos}
+        renderItem={({ item }) => {
+          return <Image Source={{ uri: item }} style={styles.img} />;
+        }}
+      />
     </Container>
   );
 };
